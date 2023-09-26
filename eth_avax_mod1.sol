e405c3745e97aa7422a23a79474f8727bb158b9d
athulya-pm <97933831+athulya-pm@users.noreply.github.com>
2023-09-26 20:17:01 +0530

    Update eth_avax_mod1.sol

--- eth_avax_mod1.sol
+++ eth_avax_mod1.sol
@@ -1,24 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-contract OwnerContract {
+contract OwnershipContract {
     address public owner;
 
     constructor() {
         owner = msg.sender;
     }
 
-    function Ownerrequire() public view {
+    function onlyOwner() public view {
         require(msg.sender == owner, "Only the owner can call this function.");
     }
 
-    function Ownerrevert() public view {
+    function onwerHere() public view {
         if(msg.sender!= owner){
             revert("The caller is not the owner.");
         }
     }
 
-    function Ownerassert() public view {
+    function Owner() public view {
         assert(msg.sender == owner);
     }
-}
